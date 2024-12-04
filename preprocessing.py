c2198454f48bb9d1db68e8d2f157a97552e270b1
Aniruddh Arora <43999650+AniruddhArora@users.noreply.github.com>
2024-12-04 17:02:40 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -18,6 +18,38 @@ def preprocessing(file_path):
     scaler = MinMaxScaler()
     X[numerical_columns] = scaler.fit_transform(X[numerical_columns])
 
+    
+    def balance_classes(X, y):
+        # count samples
+        class_counts = Counter(y)
+        max_count = max(class_counts.values())
+        
+        # X_balanced and y_balanced store the balanced classes
+        X_balanced = []
+        y_balanced = []
+
+        for cls in class_counts.keys():
+            X_class = X[y == cls]
+            y_class = y[y == cls]
+            
+            if class_counts[cls] < max_count:
+                multiplier = max_count // class_counts[cls]
+                remainder = max_count % class_counts[cls]
+                X_upsampled = pd.concat([X_class] * multiplier + [X_class.sample(remainder, replace=True)])
+                y_upsampled = pd.concat([y_class] * multiplier + [y_class.sample(remainder, replace=True)])
+            else:
+                X_upsampled = X_class
+                y_upsampled = y_class
+            
+            X_balanced.append(X_upsampled)
+            y_balanced.append(y_upsampled)
+        
+        X_balanced = pd.concat(X_balanced)
+        y_balanced = pd.concat(y_balanced)
+        return X_balanced, y_balanced
+
+    X_balanced, y_balanced = balance_classes(X, y)
+
     # Split the data into training and testing sets (80/20 split)
     X_train, X_test, y_train, y_test = train_test_split(
         X_resampled, y_resampled, test_size=0.2, random_state=42
