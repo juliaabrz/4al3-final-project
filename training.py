a38d69ceee6a4851a620da6b324f30acde40ec57
Aniruddh Arora <43999650+AniruddhArora@users.noreply.github.com>
2024-12-20 10:28:21 -0500

    Update training.py

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler, StandardScalar
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import torch
